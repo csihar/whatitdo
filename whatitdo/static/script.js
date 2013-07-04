7b25ae6a4ac952b15fc8f9bf5b0ce74cbb38722b
csihar <ksanderspdx@gmail.com>
2013-07-03 18:12:20 -0700

    Version 1.1 (responsiveness features & demo page added)

--- whatitdo/static/script.js
+++ whatitdo/static/script.js
@@ -3,46 +3,36 @@
 $(document).ready(function(){
 
 	// hide drawers - visible by default in case Javascript is off
-	$('.drawer').children().hide;
 	$('.drawer').hide();
 
 	// fade demo blurb in
-	$('#demoblurb').hide();
-	$('#demoblurb').delay(300).fadeIn(1000);
-
+	$('#demoblurb').ready(function() {
+		$('#demoblurb').hide();
+		$('#demoblurb').delay(300).fadeIn(1000);
+	});
 
 	// show/hide individual drawers
 	$('.morecol').click(function(){
 		if ($(this).hasClass('is_open')) {
-			$(this).next().toggle();
-			$(this).next().children().fadeOut(0);
+			$(this).next().fadeToggle(0);
 			$(this).removeClass('is_open');
 			return;
 		}
-		$(this).next().toggle();
-		if ($(window).width() > 888 ) {
-			$(this).next().contents(':hidden').fadeIn(350); }
-		else {$(this).next().contents(':hidden').show(); }
-
+		$(this).next().fadeToggle(300);
 		$(this).addClass('is_open');
 	});
 
 	// show/hide all drawers
 	$('.listhead.morecol').click(function(){
 		if ($(this).hasClass('all_open')) {
-			$('.drawer').hide();
-			$('.drawer').children().fadeOut(0);
+			$('.drawer').fadeIn(0).fadeToggle(0);
 			$(this).removeClass('all_open');
 			$('.morecol').removeClass('is_open');
 			return;
 		}
-		$('.drawer').show();
-		if ($(window).width() > 888 ) {
-			$('.drawer').children(':hidden').fadeIn(350); }
-		else {$('.drawer').children(':hidden').show();}
+		$('.drawer').fadeOut(0).fadeToggle(300);
 		$(this).addClass('all_open');
 		$('.morecol').addClass('is_open');
 	});
 
 });
-
