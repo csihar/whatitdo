4eae3d77e899c0e0ba452b5040bfaf12ef11cc6e
csihar <ksanderspdx@gmail.com>
2015-10-22 18:37:34 -0700

    Allow items to be filtered by item_creator or category via API

--- items/api.py
+++ items/api.py
@@ -1,6 +1,6 @@
 from tastypie.authentication import Authentication
 from tastypie.authorization import DjangoAuthorization
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from models import Item
 
 class ItemResource(ModelResource):
@@ -8,6 +8,10 @@ class ItemResource(ModelResource):
 		queryset = Item.objects.all()
 		resource_name = 'item'
 		allowed_methods = ['post', 'get', 'patch', 'delete']
+		filtering = {
+			"item_creator": ALL,
+			"category": ALL,
+		}
 		always_return_data = True
 		authentication = Authentication()
 		authorization = DjangoAuthorization()
